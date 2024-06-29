ba8b774d469ac298679bc90d9e26f364f36b964e
Vladislav Treshchev <letsgovlad@gmail.com>
2024-06-29 15:35:17 +0300

    Created the full Game

--- WordJumbleLearning/WordJumbleLearning.cpp
+++ WordJumbleLearning/WordJumbleLearning.cpp
@@ -45,11 +45,41 @@ int main()
 	{
 		int Index_1 = (rand() % Length);
 		int Index_2 = (rand() % Length);
-	}
+	
 	int Temp = Jumble[Index_1];
 	Jumble[Index_1] = Jumble[Index_2];
 	Jumble[Index_2] = Temp;
-	
+	}
+
+	std::cout << "\t\t\tWelcome to Word Jumble!\n\n";
+	std::cout << "Unscramble the letters to make a word.\n";
+	std::cout << "Enter 'hint' for a hint.\n";
+	std::cout << "Enter 'quit' to quit the game.\n\n";
+	std::cout << "The jumble is: " << Jumble;
+	std::string Guess;
+	std::cout << "\n\nYour guess: ";
+	std::cin >> Guess;
+
+	while ((Guess != TheWord) && (Guess != "quit"))
+	{
+		if (Guess == "hint")
+		{
+			std::cout << TheHint;
+		}
+		else
+		{
+			std::cout << "Sorry, that's not it";
+		}
+		std::cout << "\n\nYour guess: ";
+		std::cin >> Guess;
+	}
+
+	if (Guess == TheWord)
+	{
+		std::cout << "\nThat's it! You guessed it!\n";
+	}
+	std::cout << "\nThanks for playing!\n";
+	return 0;
 
 
 	/*enum difficulty
